d1c3b96e3d06ef300a4652fb07f0ef2ff69e352c
Amrit <tamangamrit37@gmail.com>
2022-12-08 17:10:28 +0100

    mobile responsive
    responsive design and user information

--- src/components/Photo.jsx
+++ src/components/Photo.jsx
@@ -6,33 +6,34 @@ const Photo = ({ data,  modelimg, modelset }) => {
   // const [model, setModel] = useState("");
   // const [openModel, setOpenModel] = useState(false);
 
-  const handleClick = () => {
+  const handleClick = (e) => {
+    e.preventDefault()
     modelimg(data)
     modelset(true)
     // setOpenModel(true);
   }
   return (
-    <div onClick={handleClick} className="relative peer font-sans cursor-pointer">
+    <div className="relative peer font-sans cursor-pointer">
       <div className="absolute top-2 left-3">
         <div className="flex items-center justify-between gap-2">
-          <div className="flex items-center justify-center gap-2 text-white font-bold bg-black/50 rounded-2xl p-1 transition-transform hover:scale-110 lg:hover:scale-105">
+          <div className="flex items-center justify-center gap-2 text-white font-bold bg-black/50 rounded-2xl p-1 transition-transform hover:scale-110 lg:hover:scale-x-110">
             <img src={data.userImageURL} alt="" className="w-10 rounded-full" />
             {data.user}
           </div>
         </div>
       </div>
-      <img src={data.webformatURL} key={data.id} alt={data.tags}></img>
+      <img src={data.webformatURL} key={data.id} alt={data.tags} onClick={handleClick} className="w-full"></img>
       <div className="bg-blue-300 font-bold px-2 rounded-b-3xl">
-        <div className="flex items-center justify-between">
-          <div className="flex items-center gap-1 p-1 rounded-xl transition-transform hover:scale-110 lg:hover:scale-105">
+        <div className="flex items-center justify-around">
+          <div className="flex items-center gap-1 p-1 rounded-xl">
             <AiOutlineHeart size={25}/>
             {data.likes}
           </div>
-          <div className="flex items-center p-1 gap-1 rounded-xl transition-transform hover:scale-110 lg:hover:scale-105">
+          <div className="flex items-center p-1 gap-1 rounded-xl">
             <AiOutlineComment size={25}/>
             {data.comments}
           </div>
-          <div className="flex items-center p-1 gap-1 rounded-xl transition-transform hover:scale-110 lg:hover:scale-105">
+          <div className="flex items-center p-1 gap-1 rounded-xl">
             <GrView size={25}/>
             {data.views}
           </div>
@@ -43,7 +44,7 @@ const Photo = ({ data,  modelimg, modelset }) => {
             Like
           </div>
           <div className="flex items-center p-1 gap-1 rounded-xl transition-transform hover:scale-110 lg:hover:scale-105">
-            Comment
+            Download
           </div>
           <div className="flex items-center p-1 gap-1 rounded-xl transition-transform hover:scale-110 lg:hover:scale-105">
             Share
